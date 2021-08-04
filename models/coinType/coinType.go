cd99aea927f5c9c4a9f00ff48e46510fdb0bc4fa
Alvin Wong <alvinwong312@gmail.com>
2021-08-04 03:05:23 -0400

    cointype

---
+++ models/coinType/coinType.go
@@ -0,0 +1,67 @@
+package coinType
+
+import "encoding/json"
+
+type CoinType int
+
+const (
+	ETHEUREM CoinType = iota
+	BITCOIN
+	NOT_A_COIN
+)
+
+func getSupportedCoinTypes() []CoinType{
+	coinTypes := []CoinType{ETHEUREM}
+	return coinTypes
+}
+
+func (c CoinType) String() string {
+	return [...]string{"Etheurem", "Bitcoin" ,"NotACoin"}[c]
+}
+
+func (c CoinType) Repr() string {
+	return [...]string{"60'", "0'" ,"-1"}[c]
+}
+
+func GetCoinType(repr string) CoinType {
+	switch repr {
+		case "60'":
+			return ETHEUREM
+		case "0'":
+			return BITCOIN
+		default:
+			return NOT_A_COIN 
+	}
+}
+
+func (c CoinType) MarshalJSON() ([]byte, error) {
+    return []byte(`"` + c.Repr() + `"`), nil
+}
+
+func (this CoinType) UnmarshalJSON(b []byte) error {
+	var c string
+	err := json.Unmarshal(b, &c)
+	if err != nil {
+		return err
+	}
+	this = GetCoinType(c)
+	return nil
+}
+
+type UnsupportedCoinError struct {
+	Message string
+}
+
+func (e *UnsupportedCoinError) Error() string {
+	return e.Message
+}
+
+func (c CoinType) CheckSupportCompatability() bool {
+	for _, coinType := range getSupportedCoinTypes() {
+		if c == coinType {
+			return true
+		}
+	}
+	return false
+}
+
