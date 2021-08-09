588046da1b0f5ef1a6f04dd4cdef28414f065f42
Alvin Wong <alvinwong312@gmail.com>
2021-08-09 00:09:28 -0400

    check wallet file existnace before loading wallet

--- services/loadWalletService.go
+++ services/loadWalletService.go
@@ -1,7 +1,11 @@
 package services
 
 import (
+	"errors"
+	"fmt"
+
 	"github.com/alvinwong12/cold-wallet/models/wallet"
+	"github.com/alvinwong12/cold-wallet/utils"
 )
 
 type LoadWalletService struct {
@@ -9,5 +13,8 @@ type LoadWalletService struct {
 }
 
 func (service *LoadWalletService) Run(serviceConfig *ServiceConfig) (interface{}, error){
+	if !utils.FileExists(serviceConfig.WalletFilePath) {
+		return nil, errors.New(fmt.Sprintf("Wallet file -> %s ,cannot be read.\n", serviceConfig.WalletFilePath))
+	}
 	return wallet.LoadWalletFromFile(serviceConfig.WalletFilePath, serviceConfig.CoinType, serviceConfig.password, serviceConfig.encrypted), nil
 }
